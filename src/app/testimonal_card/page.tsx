b9ee38646826ae0f5eb06d51a2e404a58a56e750
ksio2123 <ksio2123@gmail.com>
2024-10-30 16:27:17 -0400

    add new page for visual regression checker

---
+++ src/app/testimonal_card/page.tsx
@@ -0,0 +1,16 @@
+import { TestimonialCard } from "@/components/TestimonialCard";
+import pagestyle from "@/app/testimonal_card/testimonal_card_page.module.css";
+
+const { page } = pagestyle;
+export default () => {
+  return (
+    <div className={page}>
+      <TestimonialCard
+        name="Sarah Dole"
+        tag="sarahdole"
+        img_url="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+        text="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
+      />
+    </div>
+  );
+};
